e94f5c9b8acbd1b7e39d5bb38f4451d4e44212a4
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-03-22 16:10:48 +0600

    fetch added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.css";
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <section>
       <div className="container">
         <div className="shop row">
           <div className="shop-cart-area">
-            <h2>Lorem ipsum dolor sit amet.</h2>
+            <h2>Our Products.</h2>
+            {products.map((product) => console.log(product))}
           </div>
           <div className="order-summary">Lorem ipsum dolor sit.</div>
         </div>
